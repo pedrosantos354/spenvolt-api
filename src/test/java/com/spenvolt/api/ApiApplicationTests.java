f311b7e29f34648d121856fe000cba3662203b91
Pedro Bueno <pedro.santos354@al.unieduk.com.br>
2026-08-14 21:19:49 -0300

    Initial commit

---
+++ src/test/java/com/spenvolt/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spenvolt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
